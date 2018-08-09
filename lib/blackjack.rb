ee6bc743f4dbe4a3dd3a8778b5a9db5ff3a1829f
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-09 06:08:25 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -54,8 +54,12 @@ end
 
 def runner
   welcome 
-  initial_round
-  hit?
-  display_card_total
+  card_total = initial_round
+  until card_total > 21
+    hit?(card_total)
+    display_card_total(card_total)
+    card_total
+  end
+  end_game
 end
     
