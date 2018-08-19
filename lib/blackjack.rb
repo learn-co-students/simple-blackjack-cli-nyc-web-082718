6cb00db9c2d6bd3ef4ef323811b031f7df1976a5
Jordan Sternberg <jsternberg95@gmail.com>
2018-08-19 20:36:03 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -3,7 +3,7 @@ def welcome
 end
 
 def deal_card
- rand(1..11)
+ return rand(1..11)
 end
 
 def display_card_total(n)
@@ -15,11 +15,11 @@ def prompt_user
 end
 
 def get_user_input
-  gets
+  gets.chomp
 end
 
 def end_game(n)
-    puts "Sorry, you hit #{n} Thanks for playing!"
+    puts "Sorry, you hit #{n}. Thanks for playing!"
 end
 
 def initial_round
@@ -28,19 +28,32 @@ def initial_round
   return hand
 end
 
-def hit?
-  # code hit? here
+def hit?(current_hand)
+  prompt_user
+  response = get_user_input
+  until response == "s" || response == "h"
+  invalid_command
+  prompt_user
+  response = get_user_input
+end
+  if response == "s"
+   return current_hand
+  elsif response == "h"
+    new_hand = current_hand + deal_card
+    return new_hand
+end
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
 end
-
-#####################################################
-# get every test to pass before coding runner below #
-#####################################################
 
 def runner
-  # code runner here
+  welcome
+  hand = initial_round
+  until hand > 21
+  hand = hit?(hand)
+  display_card_total(hand)
 end
-    
+  end_game(hand)
+end
